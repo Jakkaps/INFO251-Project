71818afdd9eca9a1da7e19f66d8638e52776c4d2
Simen Kristoffersen <simekri@stud.ntnu.no>
2023-05-01 16:38:04 -0700

    Added nts-net model

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+SEED = 42
+IMAGE_HEIGHT = IMAGE_WIDTH = 224
+OUT_DIR = "out"
+N_SAMPLES = 1000 # Number of images to sample
+TEST_SIZE = 0.33 # Train/test ratio
+WANDB_PROJECT_NAME = "fgvca_aircraft"
