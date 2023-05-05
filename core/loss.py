dbf6cda845c5b472d3b8b92c7b8e06af5f7a1f85
Simen Kristoffersen <simekri@stud.ntnu.no>
2023-05-04 22:36:10 -0700

    Added notebook for test and train

--- core/loss.py
+++ core/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 def list_loss(logits, targets):
     temp = F.log_softmax(logits, -1)
@@ -8,10 +9,10 @@ def list_loss(logits, targets):
 
 
 def ranking_loss(score, targets, proposal_num=4):
-    loss = torch.autograd.Variable(torch.zeros(1))
+    loss = Variable(torch.zeros(1).cuda())
     batch_size = score.size(0)
     for i in range(proposal_num):
-        targets_p = (targets > targets[:, i].unsqueeze(1)).type(torch.float)
+        targets_p = (targets > targets[:, i].unsqueeze(1)).type(torch.cuda.FloatTensor)
         pivot = score[:, i].unsqueeze(1)
         loss_p = (1 - pivot + score) * targets_p
         loss_p = torch.sum(F.relu(loss_p))
--- settings.py
+++ settings.py
@@ -1,7 +1,18 @@
 
+# Random
 SEED = 42
+
+# Image shape
 IMAGE_HEIGHT = IMAGE_WIDTH = 224
-OUT_DIR = "out"
+
+# Output directory
+OUT_DIR = "output"
+
+# Hyperparameter tuning
 N_SAMPLES = 1000 # Number of images to sample
-TEST_SIZE = 0.33 # Train/test ratio
+TEST_SIZE = 0.4 # Train/test ratio
 WANDB_PROJECT_NAME = "fgvca_aircraft"
+
+# Model hyperparameters
+BATCH_SIZE = 8
+NUM_EPOCHS = 20
